d739e22232e9710ad90a57a1c654ec0f9f751020
MuhammadAnwarBadat <anwarbadat1997.ab@gmail.com>
2024-07-04 11:03:49 +0500

    Completed lesson-11

--- myproject/views.py
+++ myproject/views.py
@@ -1,10 +1,12 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def homepage(request):
-    # return HttpResponse("Hello World, I am home!")
+    # return HttpResponse("Hello World! I'm Home.")
     return render(request, 'home.html')
 
+
 def about(request):
-    # return HttpResponse("This is my about page")
-        return render(request, 'about.html')
+    # return HttpResponse("My About page.")
+    return render(request, 'about.html')
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,1 @@
-console.log('This is JS from your about page.')
+console.log('This is JS from your About page.')
